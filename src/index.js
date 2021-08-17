7b395c3fb42cebdb6ce7ad49c6fee07022554b8e
Max Om <max.omeleneckii@gmail.com>
2021-08-17 17:57:00 +0300

    init: add js cod in task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    //     return (n > 0 ? -1 : 1) * parseInt(n.toString().split('').reverse().join(''));
+    // };
+    if (n > 0) {
+        return (1 || -1) * parseInt(n.toString().split('').reverse().join(''));
+    } else if (n < 0) {
+        return (1 || -1) * parseInt(n.toString().split('').reverse().join(''));
+    }
+};
